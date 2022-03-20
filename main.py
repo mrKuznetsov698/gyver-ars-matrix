003e936817d3d8a388e96972559a38d6feaf637e
arsenii <mr.kuznetsov698@gmail.com>
2022-03-20 18:30:07 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+WIDTH = 80
+HEIGHT = 45
+mx = [[0 for j in range(HEIGHT)] for i in range(WIDTH)]
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("static/index.html")
+
+
+class StaticHandler(tornado.web.RequestHandler):
+    def get(self):
+        try:
+            MyPath = os.path.abspath('.')
+            self.write(open(MyPath+self.request.uri.replace('/', '\\'), 'rb').read())
+        except BaseException as ex:
+            self.write('404 File not found')
+
+
+class WebSocket(tornado.websocket.WebSocketHandler):
+    connections = set()
+
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        self.connections.add(self)
+
+    def on_message(self, message):
+        if message == 'initial update':
+            self.write_message('IU' + str(mx))
+            return
+        try:
+            spl = message.split(',')
+            mx[int(spl[1])][int(spl[2])] = int(spl[0])
+            [client.write_message(f"2,{spl[0]},{spl[1]},{spl[2]}") for client in self.connections]
+        except:
+            return
+        # [client.write_message('You send: ' + message) for client in self.connections]
+
+    def on_close(self):
+        self.connections.remove(self)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/static/.*", StaticHandler),
+        (r"/websocket", WebSocket)
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(80)
+    tornado.ioloop.IOLoop.current().start()
---
+++ static/index.js
@@ -0,0 +1,96 @@
+let ledW = 80
+let ledH = 45
+let ledS = 20
+let ledOffset = 1
+let canvas
+let Width = ledW * ledS + (ledW+1) * ledOffset
+let Height = ledH * ledS + (ledH+1) * ledOffset
+let ws
+
+class Pos{
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+function setup() {
+    canvas = createCanvas(Width, Height, P2D)
+    init_arrays()
+    noStroke()
+    document.addEventListener('mousedown', mouse)
+    ws = new WebSocket(document.baseURI.replace('http', 'ws') + 'websocket')
+    ws.onmessage = handle_message
+    ws.onopen = () => {ws.send('initial update')}
+}
+
+let leds = []
+let pos = []
+
+function draw() {
+    background(100)
+    draw_matrix()
+}
+
+function draw_matrix(){
+    for (let i = 0; i < ledW; i++){
+        for (let j = 0; j < ledH; j++){
+            fill(leds[i][j])
+            rect(pos[i][j].x, pos[i][j].y, ledS, ledS)
+        }
+    }
+}
+
+function init_arrays(){
+    for (let i = 0; i < ledW; i++) {
+        leds[i] = []
+        pos[i] = []
+        for (let j = 0; j < ledH; j++) {
+            leds[i].push(0)
+            pos[i].push(new Pos(ledOffset + i*(ledS + ledOffset), ledOffset + j*(ledS + ledOffset)))
+        }
+    }
+}
+
+function mouse(event){
+    if (mouseX <= ledOffset || mouseX >= Width-ledOffset)
+        return
+    if (mouseY <= ledOffset || mouseY >= Height-ledOffset)
+        return
+    let x
+    let y
+    for (let i = 0; i < ledW; i++) {
+        for (let j = 0; j < ledH; j++) {
+            if (mouseX >= pos[i][j].x
+                && mouseX <= pos[i][j].x + ledS
+                && mouseY >= pos[i][j].y
+                && mouseY <= pos[i][j].y + ledS) {
+                x = i
+                y = j
+            }
+        }
+    }
+    if (x != undefined && y != undefined){
+        if (!event.ctrlKey)
+            leds[x][y] = color(255, 0, 0)
+        else
+            leds[x][y] = 0
+        ws.send((leds[x][y] == 0 ? "0," : "1,") + x + ',' + y)
+    }
+}
+
+function handle_message(ms){
+    if (ms.data.startsWith('2,')){
+        let spl = ms.data.split(',')
+        leds[Number(spl[2])][Number(spl[3])] = spl[1] == '1' ? color(255, 0, 0) : 0
+    }
+    else if (ms.data.startsWith('IU')){
+        let data = ms.data.substring(2)
+        let arr
+        eval('arr = ' + data)
+        for (let i = 0; i < ledW; i++)
+            for (let j = 0; j < ledH; j++)
+                leds[i][j] = (arr[i][j] == 0 ? 0 : color(255, 0, 0))
+    }
+}
